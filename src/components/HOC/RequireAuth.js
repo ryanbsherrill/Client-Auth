bea8052be9ac6ff80e179074e35672f182654cdc
Tyge <tygesjohnso@gmail.com>
2017-09-01 13:23:29 -0600

    working on post req to server

--- src/components/HOC/RequireAuth.js
+++ src/components/HOC/RequireAuth.js
@@ -4,11 +4,15 @@ import { connect } from 'react-redux';
 export default (ComposedComponent) => {
     class RequireAuthentication extends Component {
         componentWillMount() {
+          if (!this.props.authenticated) {
+            this.props.router.push('/login')
+          }
             // Here, we want to check to see if `this.props.authenticated` is true
             // If it isn't, then redirect the user back to the /signin page
         }
 
         render() {
+
             // Here, check to see if `this.props.authenticated` is true
             // If it isn't, then we don't want this component to return anything
             // Else, render the component that was passed to this higher-order component
@@ -16,7 +20,7 @@ export default (ComposedComponent) => {
     }
 
     const mapStateToProps = (state) => {
-        return { 
+        return {
             authenticated: state.auth.authenticated
         };
     };
